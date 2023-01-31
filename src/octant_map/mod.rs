862311f4a15f1bef9fd2a40e61d300efb1a6abd5
TGRCDev <tgrc@tgrc.dev>
2023-01-30 18:57:53 -0800

    apply_tool for OctantMap
    No idea if this works yet. Time to write the polygonizer and see.

--- src/octant_map/mod.rs
+++ src/octant_map/mod.rs
@@ -1,2 +1,155 @@
+use std::{
+    collections::{ BTreeMap, BTreeSet },
+};
+use crate::{
+    tool::{ Tool, Action, IntersectType::*, },
+    utils
+};
+
 mod octant_key;
 pub use octant_key::*;
+
+#[derive(Debug)]
+pub struct OctantMap {
+    octants: BTreeMap<OctantKey, [f32; 8]>,
+    leaves: BTreeSet<OctantKey>,
+}
+
+impl Default for OctantMap {
+    #[inline(always)]
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl OctantMap {
+    pub fn new() -> Self {
+        let root_key = OctantKey::default();
+        let mut octants = BTreeMap::new();
+        let mut leaves = BTreeSet::new();
+        octants.insert(root_key, [-1.0; 8]);
+        leaves.insert(root_key);
+
+        Self {
+            octants,
+            leaves
+        }
+    }
+
+    fn has_children(&self, mut cell: OctantKey) -> bool {
+        cell.push(0);
+        self.exists(cell)
+    }
+
+    fn exists(&self, cell: OctantKey) -> bool {
+        self.octants.contains_key(&cell)
+    }
+
+    fn intersects_surface(&self, cell: OctantKey) -> bool {
+        let values = self.octants.get(&cell).expect("Non-existant cell key");
+        values.windows(2).any(|nums| nums[0].is_sign_negative() != nums[1].is_sign_negative())
+    }
+
+    fn is_collapsible(&self, cell: OctantKey) -> bool {
+        if cell.at_max_depth() || !self.has_children(cell) { return false; }
+
+        // Cell can't be collapsed if any children have children or intersect the isosurface
+        !cell.children().into_iter()
+            .any(|child| self.has_children(child) || self.intersects_surface(child))
+    }
+
+    fn collapse_cell(&mut self, cell: OctantKey) {
+        if !self.is_collapsible(cell) { return; }
+
+        let children = cell.children();
+        for child in children {
+            self.leaves.remove(&child);
+            self.octants.remove(&child);
+        }
+
+        self.leaves.insert(cell);
+    }
+
+    fn subdivide_cell(&mut self, cell: OctantKey) {
+        if cell.at_max_depth() || self.has_children(cell) { return; }
+
+        let cell_values = self.octants.get(&cell).unwrap();
+
+        let child_values = utils::subdivide_cell(cell_values);
+        cell.children().into_iter()
+            .zip(child_values.into_iter())
+            .for_each(|(child, values)| {
+                self.leaves.insert(child);
+                self.octants.insert(child, values);
+            });
+        self.leaves.remove(&cell);
+    }
+
+    pub fn apply_tool<T: Tool + ?Sized>(&mut self, tool: &T, action: Action, max_depth: u8) {
+        // Implementation 1: Recurse
+        // This implementation is to compare performance
+        // vs. NaiveOctree
+        fn apply_tool_at_octant<T: Tool + ?Sized>(this: &mut OctantMap, tool: &T, action: Action, max_depth: u8, octant: OctantKey) {
+            let depth = octant.depth();
+            let aabb = octant.aabb();
+            let scale = octant.scale();
+            
+            let mut newvals = this.octants.get(&octant).cloned().unwrap();
+            newvals.iter_mut()
+                .zip(aabb.calculate_corners().into_iter())
+                .for_each(|(value, pos)| {
+                    action.apply_value(value, tool.value(pos, scale))
+                });
+            
+            // Whether or not the new values intersect the isosurface
+            let newvals_intersect = newvals.windows(2).any(|vals| vals[0].is_sign_negative() != vals[1].is_sign_negative());
+            
+            let tool_aabb = match action {
+                Action::Place => tool.tool_aabb(),
+                Action::Remove => tool.aoe_aabb(),
+            };
+
+            // Check if subdivision is needed
+            // (This must happen BEFORE modification to avoid bad data)
+            if !octant.at_max_depth() && octant.depth() < max_depth && !this.has_children(octant) &&
+                (tool.is_convex() && (newvals_intersect || matches!(tool_aabb.intersect(aabb), ContainedBy))) ||
+                (tool.is_concave() && (newvals_intersect || !matches!(tool.aoe_aabb().intersect(aabb), DoesNotIntersect)))
+                {
+                    this.subdivide_cell(octant)
+            }
+
+            this.octants.insert(octant, newvals);
+
+            if this.has_children(octant) {
+                let children = octant.children();
+                // Recursive apply to each child cell
+                // If children already exist, they still get updated regardless
+                // of max_depth
+                children.into_iter().for_each(|child| apply_tool_at_octant(this, tool, action, max_depth, child));
+
+                // Check if collapse needed
+                if this.is_collapsible(octant) {
+                    this.collapse_cell(octant);
+                }
+            }
+        }
+
+        apply_tool_at_octant(self, tool, action, max_depth, OctantKey::default());
+    }
+}
+
+#[test]
+#[ignore]
+fn octant_map_test() {
+    use glam::Vec3;
+    use crate::tool::{ Sphere, Action };
+    let mut map = OctantMap::new();
+    let tool = Sphere::new(Vec3::ZERO, 0.3291);
+    map.apply_tool(&tool, Action::Place, 3);
+
+    for leaf in map.leaves.iter().cloned() {
+        assert!(!map.has_children(leaf))
+    }
+
+    println!("{map:?}");
+}
--- src/octant_map/octant_key.rs
+++ src/octant_map/octant_key.rs
@@ -1,6 +1,8 @@
 use bitvec::prelude::*;
 use std::ops::Range;
 
+use crate::tool::AABB;
+
 /// 64 bit index that describes the exact location of an octant
 /// in an octree of maximum depth 20 (Including an implied root at depth=0).
 /// ```text
@@ -65,6 +67,11 @@ impl OctantKey {
         // That works for me!
     }
 
+    #[inline(always)]
+    pub fn root() -> Self {
+        Default::default()
+    }
+
     pub fn depth(&self) -> u8 {
         self.0.view_bits::<LocalBits>()[Self::DEPTH_BITS_RANGE].load()
     }
@@ -85,6 +92,13 @@ impl OctantKey {
                     .fill(false);
             }
         }
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = u8> + '_ {
+        self.0.view_bits::<LocalBits>()
+            [..Self::CELL_INDEX_SIZE * self.depth() as usize]
+            .chunks_exact(3)
+            .map(|bits| bits.load())
     }
 
     pub fn get_index(&self, depth: u8) -> u8 {
@@ -130,6 +144,26 @@ impl OctantKey {
             [Self::CELL_INDEX_RANGE].store(index);
     }
 
+    pub fn child(mut self, index: u8) -> Self {
+        self.push(index);
+        self
+    }
+
+    pub fn children(&self) -> [Self; 8] {
+        assert!(!self.at_max_depth());
+
+        [
+            self.child(0),
+            self.child(1),
+            self.child(2),
+            self.child(3),
+            self.child(4),
+            self.child(5),
+            self.child(6),
+            self.child(7),
+        ]
+    }
+
     pub fn pop(&mut self) -> u8 {
         assert!(self.depth() > 0, "pop on empty OctantKey");
         let depth = self.depth();
@@ -137,11 +171,73 @@ impl OctantKey {
         self.set_depth(depth - 1);
         return index;
     }
+
+    pub fn parent(mut self) -> Self {
+        self.pop();
+        self
+    }
+
+    pub fn contains(&self, child: OctantKey) -> bool {
+        let depth = self.depth();
+        let child_depth = child.depth();
+        if depth >= child_depth {
+            return false;
+        }
+
+        let bits_range = 0..Self::CELL_INDEX_SIZE*depth as usize;
+
+        let bits = self.0.view_bits::<LocalBits>();
+        let child_bits = child.0.view_bits::<LocalBits>();
+
+        bits[bits_range.clone()].iter().eq(child_bits[bits_range].iter())
+    }
+
+    pub fn aabb(&self) -> AABB {
+        let mut aabb = AABB::ONE_CUBIC_METER;
+        self.iter().for_each(|idx| {
+            aabb = aabb.octree_child(idx);
+        });
+
+        aabb
+    }
+
+    pub fn scale(&self) -> f32 {
+        1.0 / (2u32.pow(self.depth() as u32) as f32)
+    }
+}
+
+#[test]
+fn octant_iter() {
+    let key = OctantKey::root().child(5).child(3).child(0).child(0).child(7);
+
+    assert!(key.iter().eq([5,3,0,0,7]))
+}
+
+#[test]
+fn octant_contains() {
+    let key1 = OctantKey::root().child(0).child(5);
+    let key2 = OctantKey::root().child(0).child(5).child(3).child(4);
+
+    assert!(key1.contains(key2));
+    // Keys do not contain themselves
+    assert!(!key1.contains(key1));
+
+    // Keys do not contain their parents
+    let key3 = OctantKey::root().child(0);
+    assert!(!key1.contains(key3));
+
+    // Keys do not contain siblings
+    let mut key4 = key3.child(4);
+    assert!(!key1.contains(key4));
+
+    key4.set_index(2, 5);
+    key4.push(5);
+    assert!(key1.contains(key4));
 }
 
 #[test]
 fn octant_key_from_u64() {
-    let mut key = OctantKey::default();
+    let mut key = OctantKey::root();
 
     key.set_depth(1);
     println!("Key: {:#066b}", key.0);
